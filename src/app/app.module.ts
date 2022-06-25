8efd672dbd63086866514bb9b02a4ea879aab913
Rodrigo Aguiar <rodrigo_aguiars@outlook.com>
2022-06-25 20:02:40 -0300

    Criando componente de transferências

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NovaTransferenciaComponent } from 'src/app/nova-transferencia/nova.transferencia.component';
 import { AppComponent } from './app.component';
+import { ExtratoComponent } from './extrato/extrato.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NovaTransferenciaComponent
+    NovaTransferenciaComponent,
+    ExtratoComponent
   ],
   imports: [
     BrowserModule,
