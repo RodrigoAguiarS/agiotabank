bc88efc3fbd7315730d672d7e39614eef05a764b
Rodrigo Aguiar <rodrigo_aguiars@outlook.com>
2022-06-26 10:55:14 -0300

    Criando mensagem com ngIf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
-import { NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NovaTransferenciaComponent } from 'src/app/nova-transferencia/nova.transferencia.component';
 import { AppComponent } from './app.component';
+import localePt from '@angular/common/locales/pt';
 import { ExtratoComponent } from './extrato/extrato.component';
+
+registerLocaleData(localePt, 'pt');
 
 @NgModule({
   declarations: [
@@ -15,7 +19,13 @@ import { ExtratoComponent } from './extrato/extrato.component';
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [{ provide: LOCALE_ID, useValue: 'pt' },
+  {
+    provide: DEFAULT_CURRENCY_CODE,
+    useValue: 'BRL',
+  },
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
